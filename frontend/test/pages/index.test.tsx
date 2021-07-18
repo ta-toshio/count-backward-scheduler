0bdd22855fab219676b30dbdc25d7a36a0d6006f
ta-toshio <ta.toshio.info@gmail.com>
2021-07-18 15:30:54 +0900

    Added jest

---
+++ frontend/test/pages/index.test.tsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import {render, fireEvent} from '../testUtils'
+
+describe('Sample Test', () => {
+    it('object assignment', () => {
+        const data = {one: 1};
+        data['two'] = 2;
+        expect(data).toEqual({one: 1, two: 2});
+    })
+})
