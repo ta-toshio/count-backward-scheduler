97d0e1afb836c0dd8b9be9deeeffcc60ba5c0ced
ta-toshio <ta.toshio.info@gmail.com>
2021-07-20 14:31:03 +0900

    Added graphql client wrapper

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { Provider } from 'react-redux'
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
 import store from '../app/store'
+import { useApollo } from '../app/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Provider>
   )
 }
