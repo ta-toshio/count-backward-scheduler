8ffd3c40c5edfd58870604ba0f943d35c1b7b611
ta-toshio <ta.toshio.info@gmail.com>
2021-07-26 17:20:47 +0900

    Add dummy top design

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -8,58 +8,76 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+  <>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/login">
-          <a>Login</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/logged-in">
-          <a>Logged In</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/register">
-          <a>Register</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/count">
-          <a>Count</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/use-apollo">
-          <a>Users List With Apollo</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/use-redux">
-          <a>Clock</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
+
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+      </div>
+
+      <div className="navbar-menu">
+        <div className="navbar-end">
+          <Link href="/">
+            <a className="navbar-item">WHY?</a>
+          </Link>
+          <Link href="/login">
+            <a className="navbar-item">Login</a>
+          </Link>
+          <Link href="/logged-in">
+            <a className="navbar-item">Logged In</a>
+          </Link>
+          <Link href="/register">
+            <a className="navbar-item">Register</a>
+          </Link>
+          <Link href="/count">
+            <a className="navbar-item">Count</a>
+          </Link>
+          <Link href="/use-apollo">
+            <a className="navbar-item">Apollo</a>
+          </Link>
+          <Link href="/use-redux">
+            <a className="navbar-item">Redux</a>
+          </Link>
+          <Link href="/uses">
+            <a className="navbar-item">Users</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
     {children}
     <footer>
-      <hr />
-      <span>I&apos;m here to stay (Footer)</span>
+      <section className="hero is-medium has-text-centered">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div data-aos="zoom-in-up" className="column is-8">
+                <h1 className="title titled is-1 mb-6">
+                  Primary bold title <span id="typewriter"></span>
+                </h1>
+                <h2 className="subtitle subtitled">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Laborum cupiditate dolorum vitae dolores nesciunt totam magni
+                  quas. Lorem ipsum, dolor sit amet consectetur adipisicing
+                  elit.
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </footer>
-  </div>
+  </>
 )
 
 export default Layout
