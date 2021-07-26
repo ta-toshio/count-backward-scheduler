a1b9a42122e9eb851fe4fc530eab60a2ab721295
ta-toshio <ta.toshio.info@gmail.com>
2021-07-26 21:12:40 +0900

    Update login form

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,71 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import Layout from '../components/Layout'
 import httpClient from '../app/httpClient'
+import Yup from '../app/yup'
+
+const schema = Yup.object().shape({
+  email: Yup.string().required().email(),
+  password: Yup.string().required().min(6),
+})
 
 const Login: NextPage = () => {
   const router = useRouter()
-  const [formData, setFormData] = useState<{
-    email: string
-    password: string
-    emailError: string
-    passwordError: string
-  }>({
-    email: 'test@test.com',
-    password: 'password',
-    emailError: '',
-    passwordError: '',
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   })
-
-  /**
-   * Handle input change.
-   *
-   * @param {object} e
-   *   The event object.
-   */
-  const handleInputChange = (e: React.FormEvent<HTMLInputElement>): void => {
-    setFormData({
-      ...formData,
-      [e.currentTarget.name]: e.currentTarget.value,
-      emailError: '',
-      passwordError: '',
-    })
-  }
 
   /**
    * Submit the form.
    */
-  const submit = (): Promise<void> => {
-    const { email, password } = formData
-
-    // Check for valid email address.
-    if (!email) {
-      setFormData({
-        ...formData,
-        emailError: 'Please provide a valid email address',
-      })
-      return
-    }
-
-    // Check for valid password.
-    if (!password) {
-      setFormData({
-        ...formData,
-        passwordError: 'Please provide a valid password',
-      })
-      return
-    }
-
+  const submit = (data): Promise<void> => {
     // Make API call if everything is fine.
     // props.login(email, password);
-    httpClient.get('/sanctum/csrf-cookie').then((_) => {
+    return httpClient.get('/sanctum/csrf-cookie').then((_) => {
       // Login...
       httpClient
         .post('/login', {
-          email: email,
-          password: password,
+          email: data.email,
+          password: data.password,
         })
         .then((_) => {
           router.push('/')
@@ -78,40 +47,58 @@ const Login: NextPage = () => {
 
   return (
     <Layout>
-      <div>
-        <input
-          type="text"
-          value={formData.email}
-          placeholder="Your email address..."
-          onChange={(e) => {
-            handleInputChange(e)
-          }}
-          name="email"
-        />
-        {formData.emailError && <div>{formData.emailError}</div>}
-      </div>
-      <div>
-        <input
-          type="password"
-          value={formData.password}
-          placeholder="Your password..."
-          onChange={(e) => {
-            handleInputChange(e)
-          }}
-          name="password"
-        />
-        {formData.passwordError && <div>{formData.passwordError}</div>}
-      </div>
+      <section className="hero is-success is-fullheight">
+        <div className="hero-body">
+          <div className="login-container container has-text-centered">
+            <div className="column is-4 is-offset-4">
+              <h3 className="title has-text-black">Login</h3>
+              <hr className="login-hr" />
+              <div className="box">
+                <form onSubmit={handleSubmit(submit)}>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        {...register('email', { required: true })}
+                        type="text"
+                        className="input"
+                        placeholder="Your email address..."
+                        autoFocus
+                      />
+                      <p>{errors && errors.email?.message}</p>
+                    </div>
+                  </div>
 
-      <div>
-        <button
-          onClick={() => {
-            submit()
-          }}
-        >
-          Login
-        </button>
-      </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        {...register('password', { required: true })}
+                        type="password"
+                        className="input"
+                        placeholder="Your password..."
+                      />
+                      <p>{errors && errors.password?.message}</p>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="checkbox">
+                      <input type="checkbox" /> Remember me
+                    </label>
+                  </div>
+                  <button className="button is-block is-info is-large is-fullwidth">
+                    Login
+                    <i className="fa fa-sign-in" />
+                  </button>
+                </form>
+              </div>
+              <p className="has-text-grey">
+                <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                <a href="../">Need Help?</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
