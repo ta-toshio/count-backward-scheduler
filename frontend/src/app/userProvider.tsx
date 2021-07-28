1dabf1e27e72a130184bf21f33793afb2f4087b2
ta-toshio <ta.toshio.info@gmail.com>
2021-07-28 10:53:06 +0900

    Add wrapper to attach user in server side props

--- frontend/src/app/userProvider.tsx
+++ frontend/src/app/userProvider.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useAppDispatch } from './hooks'
 import { getUserAction } from '../features/user/userSlice'
+import { GetServerSidePropsContext } from 'next'
+import { ParsedUrlQuery } from 'querystring'
 
 const UserProvider: React.FC = ({ children }) => {
   const dispatch = useAppDispatch()
@@ -9,6 +11,40 @@ const UserProvider: React.FC = ({ children }) => {
   }, [])
 
   return <>{children}</>
+}
+
+type withAuthServerSidePropsOptions = {
+  redirect?: string
+  store: any
+}
+
+export function withUserServerSideProps(
+  callback,
+  options: withAuthServerSidePropsOptions
+) {
+  if (!options.redirect) {
+    options.redirect = '/login'
+  }
+  return async (ctx: GetServerSidePropsContext<ParsedUrlQuery>) => {
+    if (!ctx.req.headers.cookie) {
+      return {
+        redirect: {
+          destination: options.redirect,
+          permanent: false,
+        },
+      }
+    }
+
+    const getParam = {
+      headers: {
+        Cookie: ctx.req.headers.cookie,
+      },
+    }
+
+    const user = await options.store.dispatch(getUserAction({ getParam }))
+
+    return callback ? await callback(ctx, user) : { props: { user } }
+  }
 }
 
 export default UserProvider
--- frontend/src/pages/user.tsx
+++ frontend/src/pages/user.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from 'next'
 import { wrapper } from '../app/store'
 import Layout from '../components/Layout'
 import { useStore } from 'react-redux'
-import { getUserAction, selectRootUser } from '../features/user/userSlice'
+import { selectRootUser } from '../features/user/userSlice'
 import { useAppSelector } from '../app/hooks'
+import { withUserServerSideProps } from '../app/userProvider'
 
 const User: NextPage = () => {
   const { user } = useAppSelector(selectRootUser)
@@ -18,26 +19,30 @@ const User: NextPage = () => {
   )
 }
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => async (ctx) => {
-    if (!ctx.req.headers.cookie) {
-      return {
-        props: {},
-      }
-    }
-    const getParam = {
-      headers: {
-        Cookie: ctx.req.headers.cookie,
-      },
-    }
-    await store.dispatch(getUserAction({ getParam }))
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   (store) => async (ctx) => {
+//     if (!ctx.req.headers.cookie) {
+//       return {
+//         props: {},
+//       }
+//     }
+//     const getParam = {
+//       headers: {
+//         Cookie: ctx.req.headers.cookie,
+//       },
+//     }
+//     await store.dispatch(getUserAction({ getParam }))
+//
+//     console.log('State on server', store.getState())
+//
+//     return {
+//       props: {},
+//     }
+//   }
+// )
 
-    console.log('State on server', store.getState())
-
-    return {
-      props: {},
-    }
-  }
+export const getServerSideProps = wrapper.getServerSideProps((store) =>
+  withUserServerSideProps(() => ({ props: {} }), { store })
 )
 
 export default User
