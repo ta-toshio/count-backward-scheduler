41ff9003f517c49ce50eddd0a245b17d57587482
ta-toshio <ta.toshio.info@gmail.com>
2021-08-02 09:52:20 +0900

    Rename

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { useAppSelector } from '../app/hooks'
 import { selectRootUser } from '../features/user/userSlice'
-import Board from '../modules/board/templates/board'
-import Top from '../modules/landing/templates/top'
+import Board from '../modules/board/templates/Board'
+import Top from '../modules/landing/templates/Top'
 
 const IndexPage = () => {
   const { isAuthenticated } = useAppSelector(selectRootUser)
